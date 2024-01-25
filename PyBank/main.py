84274079cf07dd4e58552620516994f1a99c9b4b
spoudel977 <94360634+spoudel977@users.noreply.github.com>
2024-01-25 02:43:13 -0330

    code corrected to include month and year for greatest increase in profits and greatest decrease in profits values.

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,8 +12,6 @@ change_sum = 0
 # Set path for file
 file_path = '/Users/santoshpoudel/Desktop/University_of_Toronto/Assignments/Python Assignment/python-challenge/PyBank/Resources/budget_data.csv'
 
-
-
 # Count number of months (in column 1) - non_blank_rows and Sum values in column 2
 with open(file_path, 'r') as file:
     csv_reader = csv.reader(file)
@@ -24,10 +22,6 @@ with open(file_path, 'r') as file:
             non_blank_row_count += 1
         value = float(row[1])
         total_sum += value
-
-
-
-### Calculate Average Change
 
 # Open the CSV file
 with open(file_path, 'r') as file:
@@ -45,6 +39,8 @@ change_count = 0
 previous_value = float(rows[0][1])
 max_increase = 0
 max_decrease = 0
+max_increase_month = ""
+max_decrease_month = ""
 
 # Iterate through the rows and calculate the sum and count of changes
 for row in rows:
@@ -54,15 +50,15 @@ for row in rows:
         change_sum += change
         if change > max_increase:
             max_increase = change
+            max_increase_month = row[0]
         if change < max_decrease:
             max_decrease = change
+            max_decrease_month = row[0]
     previous_value = value
     change_count += 1
 
-
 # Calculate the average change
 average_change = change_sum / (change_count - 1)
-
 
 # Print title for answer table
 print("Financial Analysis")
@@ -78,11 +74,10 @@ print("Total :", total_sum)
 print("Average Change:", average_change)
 
 # Print Greatest Increase in Profits
-print("Greatest Increase in Profits:", max_increase)
+print("Greatest Increase in Profits:", max_increase_month, "($", max_increase, ")")
 
 # Print Greatest Decrease in Profits
-print("Greatest Decrease in Profits:", max_decrease)
-
+print("Greatest Decrease in Profits:", max_decrease_month, "($", max_decrease, ")")
 
 # Create a text file in the Analysis folder to add summary result
 file_path = os.path.join('/Users/santoshpoudel/Desktop/University_of_Toronto/Assignments/Python Assignment/python-challenge/PyBank/Analysis', "PyBank_analysis.txt")
@@ -94,8 +89,8 @@ with open(file_path, "w") as file:
     file.write(f"Total Months: {non_blank_row_count}\n")
     file.write(f"Total: ${total_sum}\n")
     file.write(f"Average Change: ${average_change}\n")
-    file.write(f"Greatest Increase in Profits: (${max_increase})\n")
-    file.write(f"Greatest Decrease in Profits: (${max_decrease})\n")
+    file.write(f"Greatest Increase in Profits: {max_increase_month} (${max_increase})\n")
+    file.write(f"Greatest Decrease in Profits: {max_decrease_month} (${max_decrease})\n")
 
 # Close the text file
 file.close()
